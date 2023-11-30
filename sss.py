cccdc782352290fb494c4d6c7e0465256e025a54
HOSSANA SUFFICIENT <69994774+cocoyamwarrior@users.noreply.github.com>
2023-11-30 01:17:28 +0100

    Update sss.py
    this has been updated to handle PDF and DOCX files

--- sss.py
+++ sss.py
@@ -1,15 +1,36 @@
 import os
-import urllib.parse
 import json
-import sys
-
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+import PyPDF2
+from docx import Document
 
-# Assuming the text files are in the 'C:/xampp/htdocs/plagiarism/' directory
-student_files = [doc for doc in os.listdir('C:/xampp/htdocs/plagiarism/Database') if doc.endswith('.txt')]
-student_notes = [open(os.path.join('C:/xampp/htdocs/plagiarism/Database', _file), encoding='utf-8').read()
-                 for _file in student_files]
+def extract_text_from_pdf(pdf_path):
+    with open(pdf_path, 'rb') as file:
+        reader = PyPDF2.PdfReader(file)
+        text = [page.extract_text() for page in reader.pages]
+    return '\n'.join(text)
+
+def extract_text_from_docx(docx_path):
+    doc = Document(docx_path)
+    return '\n'.join([para.text for para in doc.paragraphs])
+
+def read_document(file_path):
+    file_extension = os.path.splitext(file_path)[1]
+    if file_extension == '.pdf':
+        return extract_text_from_pdf(file_path)
+    elif file_extension == '.docx':
+        return extract_text_from_docx(file_path)
+    elif file_extension == '.txt':
+        with open(file_path, encoding='utf-8') as file:
+            return file.read()
+    else:
+        return None
+
+# Assuming the files are in the 'C:/xampp/htdocs/plagiarism/Database' directory
+file_dir = 'C:/xampp/htdocs/plagiarism/Database'
+student_files = [doc for doc in os.listdir(file_dir) if doc.endswith(('.txt', '.pdf', '.docx'))]
+student_notes = [read_document(os.path.join(file_dir, _file)) for _file in student_files]
 
 def vectorize(Text): return TfidfVectorizer().fit_transform(Text).toarray()
 def similarity(doc1, doc2): return cosine_similarity([doc1, doc2])
@@ -26,10 +47,10 @@ def check_plagiarism():
         del new_vectors[current_index]
         for student_b, text_vector_b in new_vectors:
             sim_score = similarity(text_vector_a, text_vector_b)[0][1]
-            if(sim_score > 0):
+            if sim_score > 0:
                 sim_score = round(sim_score, 1)
                 student_pair = sorted((os.path.splitext(student_a)[0], os.path.splitext(student_b)[0]))
-                res = (student_pair[0]+' similar to '+ student_pair[1])
+                res = (student_pair[0] + ' similar to ' + student_pair[1])
                 plagiarism_results[res] = sim_score
     api = json.dumps(plagiarism_results)
     return api
@@ -37,3 +58,4 @@ def check_plagiarism():
 if __name__ == "__main__":
     api_result = check_plagiarism()
     print(api_result)
+
